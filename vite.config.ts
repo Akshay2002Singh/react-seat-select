1b3a1a8a1dda1fe0bbd5be32ca3bc09757bb100e
Akshay2002Singh <06akshay2002@gmail.com>
2025-09-04 03:08:29 +0530

    made changes to publish module

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import path from "path";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true, // generates "types" entrypoint in package.json
+      outDir: "dist/types",
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
