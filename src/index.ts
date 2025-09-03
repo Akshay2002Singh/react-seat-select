077f77d5265dd8a3166c1da7577f1f4263817c1f
Akshay2002Singh <06akshay2002@gmail.com>
2025-09-04 04:15:23 +0530

    updated index file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+import './components/BusSeatLayoutDesigner/styles.css';
+import './components/BusSeatSelect/styles.css';
+import './components/TheaterSeatLayoutDesigner/styles.css';
+import './components/TheaterSeatSelect/styles.css';
+
 // src/index.ts
 export { BusSeatLayoutDesigner } from "./components/BusSeatLayoutDesigner/BusSeatLayoutDesigner";
 export { BusSeatSelect } from "./components/BusSeatSelect/BusSeatSelect";
